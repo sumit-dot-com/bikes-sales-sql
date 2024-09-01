a39d74e5e1f36b225c2a0d23ce68cc79e92cb7b9
sumit-dot-com <sumitsheoran319@gmail.com>
2024-09-01 11:41:19 +0530

    Delete sql ques 6.sql

--- sql ques 6.sql
+++
@@ -1,4 +0,0 @@
--- Total sales per product: Write a query to calculate the total sales amount for each product (considering quantity, list price, and discount).
-select p.product_name, (o.quantity*o.list_price)-discount as amount
-from order_items o
-join products p using(product_id)
