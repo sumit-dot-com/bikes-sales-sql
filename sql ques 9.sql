3c9d27507964d0a1766200fe688be44a5ac06c80
sumit-dot-com <sumitsheoran319@gmail.com>
2024-09-01 11:41:54 +0530

    Delete sql ques 9.sql

--- sql ques 9.sql
+++
@@ -1,6 +0,0 @@
--- Stock availability: Write a query to find the total quantity of each product available in all stores.
-select s.store_name,p.product_name,st.quantity
-from stores s
-join stocks st using(store_id)
-join products p using(product_id)
-order by store_name
