ce53fcbd19325d20a9289478d987c443e721c20d
sumit-dot-com <sumitsheoran319@gmail.com>
2024-09-01 11:38:55 +0530

    Delete sql ques 1.sql

--- sql ques 1.sql
+++
@@ -1,5 +0,0 @@
--- Find active staff: Write a query to find all active staff members and their store names.
-select s.store_name,st.first_name
-from staffs st
-join stores s using(store_id)
-where active=1
